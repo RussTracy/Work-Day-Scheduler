8fbcdf54ce5cb42b7232668b80ab22192efdd6bc
Russ <ocjade@comcast.net>
2020-12-03 09:38:46 -0700

    dynamic color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,133 @@
 var events = {};
 var currentDay = moment().format("dddd, MMMM Do");
-var hour9 = moment().set("hour", 9).format("h A")
-var hour10 = moment().set("hour", 10).format("h A")
-var hour11 = moment().set("hour", 11).format("h A")
-var hour12 = moment().set("hour", 12).format("h A")
-var hour13 = moment().set("hour", 13).format("h A")
-var hour14 = moment().set("hour", 14).format("h A")
-var hour15 = moment().set("hour", 15).format("h A")
-var hour16 = moment().set("hour", 16).format("h A")
-var hour17 = moment().set("hour", 17).format("h A")
+var currentTime;
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment().hour(13);
+var hour14 = moment().hour(14);
+var hour15 = moment().hour(15);
+var hour16 = moment().hour(16);
+var hour17 = moment().hour(17);
+var hour18 = moment().hour(18);
+var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
 
 $("#currentDay").text(currentDay);
 
-$(document).ready(function () {
-    var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
-    $.each(hours, function (index, value) {
-        $(".container").append("<div class='row'><div class='col-2 hour text-right'>" +
-            value + "</div><div class='col-8 event'><ul class='event-group' id='hour" +
-            (index + 9) + "'><li class='event-group-item'><p class='m-1'></p></li></ul></div>" +
-            "<div class='col-2 saveBtn'><i class='fas fa-save'></i></div></div></div>");
-    });
+$.each(hours, function (index, value) {
+    $(".container").append("<div class='row'><div class='col-2 hour text-right' id='hour" +
+        (index + 9) + "'><span>" + value.format("h A") + "</span></div><div class='col-8 event' id='timeblock" +
+        (index + 9) + "'><ul class='event-group' id='event" +
+        (index + 9) + "'><li class='event-group-item'><p class='m-1'></p></li></ul></div>" +
+        "<div class='col-2 saveBtn'><i class='fas fa-save'></i></div></div></div>");
 
 });
+var auditTime = function () {
+    currentTime = moment().format("h:m:s A");
+    if (moment().isBetween(hour9, hour10)) {
+        $("#timeblock9").addClass("present");
+    }
+    else if (moment().isAfter(hour10)) {
+        $("#timeblock9").addClass("past");
+    }
+    else {
+        $("#timeblock9").addClass("future");
+    }
 
+
+    if (moment().isBetween(hour10, hour11)) {
+        $("#timeblock10").addClass("present");
+    }
+    else if (moment().isAfter(hour11)) {
+        $("#timeblock10").addClass("past");
+    }
+    else {
+        $("#timeblock10").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour11, hour12)) {
+        $("#timeblock11").addClass("present");
+    }
+    else if (moment().isAfter(hour12)) {
+        $("#timeblock11").addClass("past");
+    }
+    else {
+        $("#timeblock11").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour12, hour13)) {
+        $("#timeblock12").addClass("present");
+    }
+    else if (moment().isAfter(hour13)) {
+        $("#timeblock12").addClass("past");
+    }
+    else {
+        $("#timeblock12").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour13, hour14)) {
+        $("#timeblock13").addClass("present");
+    }
+    else if (moment().isAfter(hour14)) {
+        $("#timeblock13").addClass("past");
+    }
+    else {
+        $("#timeblock13").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour14, hour15)) {
+        $("#timeblock14").addClass("present");
+    }
+    else if (moment().isAfter(hour15)) {
+        $("#timeblock14").addClass("past");
+    }
+    else {
+        $("#timeblock14").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour15, hour16)) {
+        $("#timeblock15").addClass("present");
+    }
+    else if (moment().isAfter(hour16)) {
+        $("#timeblock15").addClass("past");
+    }
+    else {
+        $("#timeblock15").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour16, hour17)) {
+        $("#timeblock16").addClass("present");
+    }
+    else if (moment().isAfter(hour17)) {
+        $("#timeblock16").addClass("past");
+    }
+    else {
+        $("#timeblock16").addClass("future");
+    }
+
+
+    if (moment().isBetween(hour17, hour18)) {
+        $("#timeblock17").addClass("present");
+    }
+    else if (moment().isAfter(hour18)) {
+        $("#timeblock17").addClass("past");
+    }
+    else {
+        $("#timeblock17").addClass("future");
+    };
+}
+
+// This function runs but the time is never refreshed?
+setInterval(function () {
+    $(".event").each(function (index, el) {
+        auditTime(el);
+        console.log(currentTime);
+    });
+}, (1000 * 60)); // 1000ms x 60 = 1 minute x 30 = 30 minutes
